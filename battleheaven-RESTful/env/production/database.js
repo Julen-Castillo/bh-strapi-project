379dc63a335b2474ed481c0bed060cf01de18c93
Julen <castillo@kindairesearch.com>
2023-01-14 03:34:07 +0100

    init

---
+++ battleheaven-RESTful/env/production/database.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', 'db-battleheaven-do-user-13276797-0.b.db.ondigitalocean.com'),
+      port: env.int('DATABASE_PORT', 25060),
+      database: env('DATABASE_NAME', 'defaultdb'),
+      user: env('DATABASE_USERNAME', 'doadmin'),
+      password: env('DATABASE_PASSWORD', 'AVNS_eHvYjqZ4MSXvUCEh8qM'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+    },
+    debug: false,
+  },
+});
